fb928fc993b150e31582a5941aa70f9152d76635
JoelOdhiambo <wesleyjoel4@gmail.com>
2022-04-12 12:40:41 +0300

    Stack and Queue

---
+++ queue.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+class QueueDeque:
+    def __init__(self):
+        self.queue=deque()
+    
+    def enqueue(self,item):
+        self.queue.append(item)
+        
+    def dequeue(self):
+        if(len(self.queue)>0):
+            self.queue.popleft()
+        else: 
+            return None
+        
+    def front(self):
+        if(len(self.queue)>0):
+            return self.queue[0]
+        else:
+            return None
+    
+    def rear(self):
+        if(len(self.queue)>0):
+            return self.queue[len(self.queue)-1]
+        else:
+            return None
+        
+    def __str__(self) -> str:
+        return str(self.queue)
+         
+
+if __name__ == '__main__':
+    queue=QueueDeque()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.dequeue()
+    print(queue.__str__())
---
+++ stack.py
@@ -0,0 +1,63 @@
+from collections import deque
+
+
+class StackList:
+    def __init__(self):
+        self.stack = list()
+    def push(self,item):
+        self.stack.append(item)
+    def pop(self):
+        if len(self.stack)>0:
+          return self.stack.pop()
+        else:
+          return None
+    def peek(self):
+        if len(self.stack)>0:
+            return self.stack[len(self.stack)-1]
+        else:
+            return None
+    def __str__(self):
+        return str(self.stack)
+
+class StackDeque:
+    def __init__(self):
+        self.stack=deque()
+        
+    def push(self,item):
+        self.stack.append(item)
+        
+    def pop(self):
+        if(len(self.stack)>0):
+            return self.stack.pop()
+        else:
+            return None
+        
+    def peek(self):
+        if(len(self.stack)>0):
+            return self.stack[len(self.stack)-1]
+        else:
+            return None
+    
+    def is_empty(self):
+        if(len(self.stack)>0):
+            return False
+        else:
+            return True
+    
+    def __str__(self) -> str:
+        return str(self.stack)
+    
+    
+    
+
+if __name__ == '__main__':
+    stack=StackDeque()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.pop()
+
+    print(stack.is_empty())
+        
+    
+
